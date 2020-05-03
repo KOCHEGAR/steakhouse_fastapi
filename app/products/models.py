330f7b9488f976e1192e5ccc922b0b54b687f9d1
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-03 17:59:11 +1000

    product mongoengine model

--- app/products/models.py
+++ app/products/models.py
@@ -1,6 +1,18 @@
-from mongoengine import Document, BooleanField, FloatField, ReferenceField, IntField
+from mongoengine import Document, StringField, BooleanField, FloatField, ReferenceField, IntField
+from app.enums import CookRoles
 
 
 class Product(Document):
-    pass
+    title = StringField(required=True)
+    img = StringField(default='default.jpg')
+    price = FloatField(required=True)
+    barcode = StringField(required=True)
+    product_code = StringField(required=True)
+    weight = IntField(required=True)
+    unit = StringField(default='г')
+    description = StringField(default='Place for description...')
+    status_active = BooleanField(default=True)
+    cook_sign = StringField(default=CookRoles.steak)
+    tax_rate = FloatField(default=20)
 
+    meta = {'collection': 'products', 'strict': False}

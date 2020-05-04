d42be1c17fd8f3641f5794130455ae51b119643b
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-05 01:20:39 +1000

    english? Ok!

--- app/orders/doc_info.py
+++ app/orders/doc_info.py
@@ -1,19 +1,30 @@
+from typing import List
 from starlette.status import HTTP_201_CREATED
 from app.helpers import DocInfo
+from .schemes import ResponseCreateOrder, ResponseGetOrder
 
+
+doc_get_orders = DocInfo(
+    descr='Get list of orders',
+    summ='Get orders',
+    res_descr='List of orders',
+    # resp_model=List[ResponseGetOrder]
+).__dict__
 
 doc_create_order = DocInfo(
-    descr='Операция создания заказа в системе. Формат времени order_time: ',
-    summ='Создание заказа',
-    res_descr='Заказ создан успешно',
+    descr='Order creation. Example of ime format for order_time: ',
+    summ='Create order',
+    res_descr='Order created successfully',
     status_code=HTTP_201_CREATED,
+    resp_model=ResponseCreateOrder
 ).__dict__
 
 
 doc_get_order_by_id = DocInfo(
-    descr='Получение заказа по ID',
-    summ='Получение заказа по ID',
-    res_descr='Заказ',
+    descr='Get order by ID',
+    summ='Get order by ID',
+    res_descr='Retrieved order',
+    resp_model=ResponseGetOrder
 ).__dict__
 
 
@@ -22,14 +33,14 @@ allowed_fields_to_update = [
 ]
 
 doc_update_order = DocInfo(
-    descr=f'Обновление заказа по ID. Обновлять можно только поля: {allowed_fields_to_update}',
-    summ='Обновление заказа',
-    res_descr='Заказ обновлен'
+    descr=f'Update order by ID. Allowed fields to update: {allowed_fields_to_update}',
+    summ='Update order',
+    res_descr='Order updated'
 ).__dict__
 
 
 doc_delete_order = DocInfo(
-    descr='Удаление заказа по ID',
-    summ='Удаление заказа',
-    res_descr='Заказ удалён'
+    descr='Delete order by ID',
+    summ='Delete order',
+    res_descr='Order deleted'
 ).__dict__

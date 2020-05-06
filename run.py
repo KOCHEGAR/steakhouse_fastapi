1f7a7cf484b693d875d5261443d11c0a163f4bd5
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-07 01:34:05 +1000

    rename env vars for dev

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     uvicorn.run(app='app:app',
                 host='0.0.0.0',
                 port=8044,
-                reload=bool(envs.get('RELOAD', False)),
-                debug=bool(envs.get('DEBUG', False)),
+                reload=bool(envs.get('ST_RELOAD', False)),
+                debug=bool(envs.get('ST_DEBUG', False)),
                 loop='asyncio')

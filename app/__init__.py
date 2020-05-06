140bff2642a163b3154a4390fd8667097ccf0031
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-07 01:32:41 +1000

    added connect to mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from config import config
 from fastapi import FastAPI
+from mongoengine import connect, disconnect
 
 from .orders.routes import orders_router
 from .cart.routes import cart_router
@@ -15,3 +16,21 @@ app.include_router(cart_router, prefix='/api', tags=['Cart operations'])
 app.include_router(types_router, prefix='/api', tags=['Types operations'])
 app.include_router(subtypes_router, prefix='/api', tags=['Subtypes operations'])
 app.include_router(products_router, prefix='/api', tags=['Products operations'])
+
+connect(
+    username=config['MONGO_USER'],
+    password=config['MONGO_PASSWORD'],
+    host=config['MONGO_HOST'],
+    port=config['MONGO_PORT'],
+    db=config['MONGO_DBNAME']
+)
+
+
+@app.on_event('startup')
+def startup():
+    pass
+
+
+@app.on_event('shutdown')
+def shutdown():
+    disconnect()

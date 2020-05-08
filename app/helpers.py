a08a8b90bbbb86708d6ce943ede89ad72182d6e4
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-09 03:33:40 +1000

    new type for pydantic and refactor paginate_model func to work with marshmallow

--- app/helpers.py
+++ app/helpers.py
@@ -1,15 +1,35 @@
+from bson import ObjectId
+from bson.errors import InvalidId
 from pydantic import BaseModel, Field
 from fastapi import Query
 
 
 # For easy building openapi arguments for endpoints
 class DocInfo:
-    def __init__(self, descr='', summ='', res_descr='', status_code=200, resp_model=None):
+    def __init__(self, descr='', summ='', res_descr='', status_code=200, resp_model=None, **kwargs):
         self.description = descr
         self.summary = summ
         self.response_description = res_descr
         self.status_code = status_code
         self.response_model = resp_model
+        # self.response_model_exclude_unset = True
+
+        # for key, val in kwargs.items():
+        #     setattr(self, key, val)
+
+
+class ObjectIdStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        try:
+            ObjectId(str(v))
+        except InvalidId:
+            raise ValueError("Not a valid ObjectId")
+        return str(v)
 
 
 class PaginatedResult(BaseModel):
@@ -29,29 +49,29 @@ class Pag(BaseModel):
         return {'pagination_data': data}
 
 
-def paginate_model(pagination, mongo_model):
+def paginate_model(pagination, marshmallow_model):
     page = pagination['offset']
     limit = pagination['limit']
     fields = str(pagination['select_fields']).split(',')
     start = (page - 1) * limit
     stop = page * limit
 
+    mongo_model = marshmallow_model.Meta.model
     models = mongo_model.objects()[start:stop]
     total_count_of_models = models.count()
+    marshmallow_kwargs = {'many': True}
 
     if fields:
         model_fields = list(mongo_model._fields.keys())
-        valid_fields = []
         for field in fields:
             if field in model_fields:
-                valid_fields.append(field)
-        if valid_fields:
-            models = models.only(*valid_fields)
+                marshmallow_kwargs.setdefault('only', []).append(field)
 
+    models_to_response = marshmallow_model(**marshmallow_kwargs).dump(models).data
     pagination_result = {
         'next': page + 1 if stop < total_count_of_models else -1,
         'prev': page - 1 if start > 0 and stop < total_count_of_models else -1,
         'total': total_count_of_models,
-        'result': list(models)
+        'result': models_to_response
     }
     return pagination_result

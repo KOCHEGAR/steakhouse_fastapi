f766a4394685447eafaf8b782b75c75448b703b7
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-09 22:12:48 +1000

    subtype model

---
+++ app/subtypes/models.py
@@ -0,0 +1,29 @@
+
+from mongoengine import Document, StringField, BooleanField, ReferenceField, ListField, PULL
+from app.products.models import Product, ProductMarshmallow
+from marshmallow_mongoengine import ModelSchema, fields
+
+
+class Subtype(Document):
+    title = StringField(required=True)
+    status = BooleanField(default=False)
+    products = ListField(ReferenceField(Product, reverse_delete_rule=PULL), default=[])
+
+    meta = {'collection': 'subtypes', 'strict': False}
+
+    def _check_presence(self, product_instance):
+        return product_instance in self.products
+
+    def add_product(self, product_instance):
+        self.products.append(product_instance)
+        self.save()
+
+    def remove_product(self, product_instance):
+        pass
+
+
+class SubtypeMarshmallowModel(ModelSchema):
+    products = fields.Nested(ProductMarshmallow, many=True)
+
+    class Meta:
+        model = Subtype

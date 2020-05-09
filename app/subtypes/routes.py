6df310d22271d864dbc24e0699fbd6df1c1353a2
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-09 22:13:38 +1000

    some routes on subtypes

--- app/subtypes/routes.py
+++ app/subtypes/routes.py
@@ -1,12 +1,16 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from . import functions as funcs
+from .doc_info import doc_get_subtype, doc_get_subtypes
+
+from app.helpers import Pag
 
 subtypes_router = APIRouter()
 
 
-@subtypes_router.get('/subtypes')
-def get_subtypes():
-    pass
+@subtypes_router.get('/subtypes', **doc_get_subtypes)
+def get_subtypes(pag: Pag = Depends()):
+    return funcs.get_subtypes(**pag.dict())
 
 
 @subtypes_router.post('/subtypes')
@@ -14,9 +18,9 @@ def create_subtype():
     pass
 
 
-@subtypes_router.get('/subtypes/{subtype_id}')
-def get_subtype(subtype_id):
-    pass
+@subtypes_router.get('/subtypes/{subtype_id}', **doc_get_subtype)
+def get_subtype(subtype_id: str):
+    return funcs.get_subtype(subtype_id)
 
 
 @subtypes_router.put('/subtypes/{subtype_id}')

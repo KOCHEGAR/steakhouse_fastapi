23235d7cc014b46b8d16a27b8612f5ea31b047ba
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-09 22:10:31 +1000

    some improvements

--- app/products/doc_info.py
+++ app/products/doc_info.py
@@ -1,15 +1,12 @@
 from typing import List
-from .schemes import Product, ProductForGetProducts
-from app.helpers import DocInfo, PaginatedResult
-
-
-class ListOfProducts(PaginatedResult):
-    result: List[ProductForGetProducts]
+from .schemes import ListOfProducts
+from app.helpers import DocInfo
 
 
 doc_get_products = DocInfo(
     descr='Get list of products',
     summ='Get products',
     res_descr='List of products',
-    resp_model=ListOfProducts
+    resp_model=ListOfProducts,
+    response_model_exclude_unset=True
 ).__dict__

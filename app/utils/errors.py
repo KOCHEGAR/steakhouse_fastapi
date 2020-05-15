5816808979383737066b00436476fbc5594020ea
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-16 01:27:44 +1000

    new exception
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,7 +1,10 @@
+
 from fastapi.exceptions import RequestValidationError, HTTPException
 
+from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 
 def http_error_handler(request: Request, exc: HTTPException) -> JSONResponse:
@@ -16,4 +19,10 @@ def handle_422(req: Request, exc: RequestValidationError):
         err_field_name = err['loc'][-1]
         err_field_value = exc.body[err_field_name]
         errs['errors'].append({err_field_name: f"({err_field_value}) - {err['msg']}"})
-    return JSONResponse(errs, status_code=422)
+    return JSONResponse(errs, status_code=HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+class SteakhouseException(HTTPException):
+    def __init__(self, err_str, code=400):
+        self.status_code = code
+        self.detail = err_str

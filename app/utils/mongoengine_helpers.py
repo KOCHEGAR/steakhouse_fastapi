9b36c21604f51db742de5a3cd591e64ed208615d
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-17 00:00:47 +1000

    rework function
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/utils/mongoengine_helpers.py
+++ app/utils/mongoengine_helpers.py
@@ -3,11 +3,11 @@ from mongoengine.errors import DoesNotExist, ValidationError
 from .errors import SteakhouseException
 
 
-def get_model_by_id_or_raise(model, model_id, no_dereference=True):
+def get_model_by_id_or_raise(model, model_id, include_nested=False):
     try:
-        if no_dereference:
-            return model.objects.no_dereference().get(id=model_id)
-        else:
+        if include_nested:
             return model.objects.get(id=model_id)
+        else:
+            return model.objects.no_dereference().get(id=model_id)
     except (DoesNotExist, ValidationError):
         raise SteakhouseException(f'No such element with id of ({model_id})')

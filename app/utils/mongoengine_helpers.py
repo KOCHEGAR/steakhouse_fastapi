442adf020b72c7b6263d6dd83ccdfb6c4151b434
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-17 05:53:50 +1000

    add nested_deep argument
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/utils/mongoengine_helpers.py
+++ app/utils/mongoengine_helpers.py
@@ -3,10 +3,10 @@ from mongoengine.errors import DoesNotExist, ValidationError
 from .errors import SteakhouseException
 
 
-def get_model_by_id_or_raise(model, model_id, include_nested=False):
+def get_model_by_id_or_raise(model, model_id, include_nested=False, nested_deep=1):
     try:
         if include_nested:
-            return model.objects.get(id=model_id)
+            return model.objects.get(id=model_id).select_related(max_depth=nested_deep)
         else:
             return model.objects.no_dereference().get(id=model_id)
     except (DoesNotExist, ValidationError):

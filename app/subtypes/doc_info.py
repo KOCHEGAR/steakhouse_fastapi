81b78759b01ffc262687ef140cc86c5344fe0ffa
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-17 00:02:51 +1000

    new docs and rewrite some old ones
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/subtypes/doc_info.py
+++ app/subtypes/doc_info.py
@@ -1,7 +1,7 @@
+from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
+
 from app.utils.documentation import DocInfo
-from starlette.status import HTTP_201_CREATED
-from .schemes import ResponseGetSubtype, ListOfSubtypes, ResponseUpdateSubtype, \
-    ResponseDeleteSubtype, ResponseCreateSubtype
+from .schemes import ResponseGetSubtype, ListOfSubtypes, ResponseCreateSubtype, ResponseGetSubtypeProducts
 
 doc_get_subtype = DocInfo(
     descr='Get Subtype',
@@ -21,15 +21,16 @@ doc_get_subtypes = DocInfo(
 doc_update_subtype = DocInfo(
     descr='Update subtype',
     summ='Update subtype',
-    res_descr='Updated subtype',
-    resp_model=ResponseUpdateSubtype
+    res_descr='Subtype is updated',
+    # resp_model=ResponseUpdateSubtype,
+    status_code=HTTP_204_NO_CONTENT
 ).__dict__
 
 doc_delete_subtype = DocInfo(
     descr='Delete subtype',
     summ='Delete subtype',
-    res_descr='ID of deleted subtype',
-    resp_model=ResponseDeleteSubtype
+    res_descr='Subtype is deleted',
+    status_code=HTTP_204_NO_CONTENT
 ).__dict__
 
 doc_create_subtype = DocInfo(
@@ -38,4 +39,25 @@ doc_create_subtype = DocInfo(
     res_descr='Created subtype',
     resp_model=ResponseCreateSubtype,
     status_code=HTTP_201_CREATED
+).__dict__
+
+doc_get_subtype_products = DocInfo(
+    descr='Get subtype products',
+    summ='Get subtype products',
+    res_descr='Products that attached to this subtype',
+    resp_model=ResponseGetSubtypeProducts
+).__dict__
+
+doc_add_product_to_subtype = DocInfo(
+    descr='Add product to subtype',
+    summ='Add product to subtype',
+    res_descr='Product is added to subtype',
+    status_code=HTTP_204_NO_CONTENT
+).__dict__
+
+doc_delete_product_from_subtype = DocInfo(
+    descr='Delete product from subtype',
+    summ='Delete product from subtype',
+    res_descr='Product is deleted from subtype',
+    status_code=HTTP_204_NO_CONTENT
 ).__dict__

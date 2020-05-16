a5bcf7faa5d286780065c604768bb5e8c83b26f0
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-16 20:49:28 +1000

    new doc_info to subtypes
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/subtypes/doc_info.py
+++ app/subtypes/doc_info.py
@@ -1,11 +1,13 @@
-from .schemes import SubtypeForGetSubtype, ListOfSubtypes
-from ..utils.documentation import DocInfo
+from app.utils.documentation import DocInfo
+from starlette.status import HTTP_201_CREATED
+from .schemes import ResponseGetSubtype, ListOfSubtypes, ResponseUpdateSubtype, \
+    ResponseDeleteSubtype, ResponseCreateSubtype
 
 doc_get_subtype = DocInfo(
     descr='Get Subtype',
     summ='Get Subtype',
     res_descr='Subtype',
-    resp_model=SubtypeForGetSubtype
+    resp_model=ResponseGetSubtype
 ).__dict__
 
 doc_get_subtypes = DocInfo(
@@ -14,4 +16,26 @@ doc_get_subtypes = DocInfo(
     res_descr='Subtypes',
     resp_model=ListOfSubtypes,
     response_model_exclude_unset=True
+).__dict__
+
+doc_update_subtype = DocInfo(
+    descr='Update subtype',
+    summ='Update subtype',
+    res_descr='Updated subtype',
+    resp_model=ResponseUpdateSubtype
+).__dict__
+
+doc_delete_subtype = DocInfo(
+    descr='Delete subtype',
+    summ='Delete subtype',
+    res_descr='ID of deleted subtype',
+    resp_model=ResponseDeleteSubtype
+).__dict__
+
+doc_create_subtype = DocInfo(
+    descr='Create subtype',
+    summ='Create subtype',
+    res_descr='Created subtype',
+    resp_model=ResponseCreateSubtype,
+    status_code=HTTP_201_CREATED
 ).__dict__

4e8e0c90229998c122e353907494a4cb7a934aa1
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-17 00:02:27 +1000

    new functions and rewrite some old ones
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/subtypes/functions.py
+++ app/subtypes/functions.py
@@ -37,7 +37,6 @@ def update_subtype(*args, **kwargs):
 
     subtype = get_model_by_id_or_raise(Subtype, item_id)
     subtype.update(**data)
-    return subtype
 
 
 def delete_subtype(*args, **kwargs):
@@ -45,19 +44,35 @@ def delete_subtype(*args, **kwargs):
 
     subtype = get_model_by_id_or_raise(Subtype, item_id)
     subtype.delete()
-    return {'id': item_id}
+
+
+def get_products_in_subtype(*args, **kwargs):
+    item_id = kwargs['id']
+    subtype = get_model_by_id_or_raise(Subtype, item_id, include_nested=True)
+    return subtype
 
 
 def add_product_to_subtype(*args, **kwargs):
-    subtype_id = kwargs['subtype_id']
-    product_id = kwargs['product_id']
-
-    product = get_model_by_id_or_raise(Product, product_id)
-    subtype = get_model_by_id_or_raise(Subtype, subtype_id, no_dereference=False)
+    subtype, product = _add_or_remove(**kwargs)
 
     if subtype.check_presence(product):
         raise SteakhouseException('That product already in this subtype')
 
+    subtype.add_product(product)
 
-def remove_product_from_subtype():
-    pass
+
+def remove_product_from_subtype(*args, **kwargs):
+    subtype, product = _add_or_remove(**kwargs)
+
+    if not subtype.check_presence(product):
+        raise SteakhouseException('That product not found in this subtype')
+
+    subtype.remove_product(product)
+
+
+def _add_or_remove(**kwargs):
+    subtype_id = kwargs['subtype_id']
+    product_id = kwargs['product_id']
+    product = get_model_by_id_or_raise(Product, product_id)
+    subtype = get_model_by_id_or_raise(Subtype, subtype_id, include_nested=True)
+    return subtype, product

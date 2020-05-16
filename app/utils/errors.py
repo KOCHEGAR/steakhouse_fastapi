c793105f69ff2969b115550338c4dabf759f841e
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-16 20:50:20 +1000

    change the way ob building error messages
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -1,5 +1,4 @@
-
-from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.exceptions import RequestValidationError
 
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
@@ -9,16 +8,25 @@ from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 def http_error_handler(request: Request, exc: HTTPException) -> JSONResponse:
     err = {exc.status_code: exc.detail}
-    return JSONResponse({"errors": [err]}, status_code=exc.status_code)
+    return JSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
 
 
 def handle_422(req: Request, exc: RequestValidationError):
     errs = {'errors': []}
 
-    for err in exc.errors():
-        err_field_name = err['loc'][-1]
-        err_field_value = exc.body[err_field_name]
-        errs['errors'].append({err_field_name: f"({err_field_value}) - {err['msg']}"})
+    print('errs ', exc.errors())
+    print('err body ', exc.body)
+    if not exc.body:
+        errs['errors'].append('Request body is empty')
+    else:
+        for err in exc.errors():
+            err_field_name = err['loc'][-1]
+            err_field_value = exc.body.get(err_field_name, err_field_name)
+
+            if err_field_name == err_field_value:
+                errs['errors'].append(f"{err_field_name} - {err['msg']}")
+            else:
+                errs['errors'].append(f"{err_field_name}: ({err_field_value}) - {err['msg']}")
     return JSONResponse(errs, status_code=HTTP_422_UNPROCESSABLE_ENTITY)
 
 

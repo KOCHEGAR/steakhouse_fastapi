1ced494f3acde6b8f034b055150f881dd1f509fd
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-16 20:55:54 +1000

    new routes and rework old ones
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/subtypes/routes.py
+++ app/subtypes/routes.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter, Depends
 
 from app.utils.pagination import Paginator
 from . import functions as funcs
-from .doc_info import doc_get_subtype, doc_get_subtypes
+from .doc_info import doc_get_subtype, doc_get_subtypes, doc_update_subtype, \
+    doc_delete_subtype, doc_create_subtype
+
+from .schemes import RequestUpdateSubtype, RequestCreateSubtype
 
 subtypes_router = APIRouter()
 
@@ -13,21 +16,25 @@ def get_subtypes(paginator: Paginator = Depends()):
     return funcs.get_subtypes(**kwargs)
 
 
-@subtypes_router.post('/subtypes')
-def create_subtype():
-    pass
+@subtypes_router.post('/subtypes', **doc_create_subtype)
+def create_subtype(data: RequestCreateSubtype):
+    data = {'data': data.dict()}
+    return funcs.create_subtype(**data)
 
 
 @subtypes_router.get('/subtypes/{subtype_id}', **doc_get_subtype)
 def get_subtype(subtype_id: str):
-    return funcs.get_subtype(subtype_id)
+    data = {'id': subtype_id}
+    return funcs.get_subtype(**data)
 
 
-@subtypes_router.put('/subtypes/{subtype_id}')
-def update_subtype(subtype_id):
-    pass
+@subtypes_router.put('/subtypes/{subtype_id}', **doc_update_subtype)
+def update_subtype(subtype_id: str, data: RequestUpdateSubtype):
+    data = {'id': subtype_id, 'data': data.dict(exclude_none=True, exclude_unset=True)}
+    return funcs.update_subtype(**data)
 
 
-@subtypes_router.delete('/subtypes/{subtype_id}')
-def delete_subtype(subtype_id):
-    pass
+@subtypes_router.delete('/subtypes/{subtype_id}', **doc_delete_subtype)
+def delete_subtype(subtype_id: str):
+    data = {'id': subtype_id}
+    return funcs.delete_subtype(**data)

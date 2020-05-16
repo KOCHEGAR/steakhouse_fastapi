198caf41a9883bbd1d582444dbd5e1161a6796b2
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-16 20:54:56 +1000

    new functions to interact with subtypes
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/subtypes/functions.py
+++ app/subtypes/functions.py
@@ -1,6 +1,8 @@
+from app.products.models import Product
 from app.utils.errors import SteakhouseException
+from app.utils.mongoengine_helpers import get_model_by_id_or_raise
 from .models import Subtype
-from .schemes import SubtypeForGetSubtypes
+from .schemes import ResponseGetSubtypes
 
 
 def create_dummy_subtypes():
@@ -9,38 +11,52 @@ def create_dummy_subtypes():
     for i in range(5):
         p = Subtype()
         p.title = f'item_{str(uuid4())}_{i}'
-        # p.barcode = str(randint(200000, 3000000))
-        # p.product_code = str(randint(400000, 500000))
-        # p.price = float(randint(20, 300))
-        # p.weight = randint(50, 1000)
         p.save()
 
 
 def get_subtypes(*args, **kwargs):
-    return kwargs['paginator_instance'].paginate_model(SubtypeForGetSubtypes, Subtype)
+    return kwargs['paginator_instance'].paginate_model(ResponseGetSubtypes, Subtype)
 
 
-def create_subtype():
-    pass
+def create_subtype(*args, **kwargs):
+    data = kwargs['data']
+    return Subtype(**data).save()
 
 
-def get_subtype(subtype_id):
-    try:
-        return Subtype.objects.get(id=subtype_id)
-    except Exception as e:
-        raise SteakhouseException(f'No such element with id ({subtype_id})')
+def get_subtype(*args, **kwargs):
+    item_id = kwargs['id']
+    return get_model_by_id_or_raise(Subtype, item_id)
 
 
-def update_subtype():
-    pass
+def update_subtype(*args, **kwargs):
+    item_id = kwargs['id']
+    data = kwargs['data']
+
+    if not data:
+        raise SteakhouseException(f'Empty data to update')
+
+    subtype = get_model_by_id_or_raise(Subtype, item_id)
+    subtype.update(**data)
+    return subtype
 
 
-def delete_subtype():
-    pass
+def delete_subtype(*args, **kwargs):
+    item_id = kwargs['id']
+
+    subtype = get_model_by_id_or_raise(Subtype, item_id)
+    subtype.delete()
+    return {'id': item_id}
 
 
-def add_product_to_subtype():
-    pass
+def add_product_to_subtype(*args, **kwargs):
+    subtype_id = kwargs['subtype_id']
+    product_id = kwargs['product_id']
+
+    product = get_model_by_id_or_raise(Product, product_id)
+    subtype = get_model_by_id_or_raise(Subtype, subtype_id, no_dereference=False)
+
+    if subtype.check_presence(product):
+        raise SteakhouseException('That product already in this subtype')
 
 
 def remove_product_from_subtype():

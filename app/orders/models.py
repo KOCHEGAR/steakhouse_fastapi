1cee2f9c9fd7c0661485b66626639d5262612499
alex <alekseydavaydosvidaniya@gmail.com>
2020-05-27 21:32:59 +1000

    some clarification
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/orders/models.py
+++ app/orders/models.py
@@ -5,8 +5,7 @@ from app.ordered_product.models import OrderedProduct
 
 
 class Order(Document):
-    on_site = BooleanField(required=True)
-    order_status = StringField(required=True)
+    # come from fiscal device
     fiscal_order_id = StringField(required=True)
     payment_type = StringField(required=True)
     payment_link = StringField(default='')
@@ -14,10 +13,12 @@ class Order(Document):
     device_id = StringField(required=True)
     title = StringField(required=True)
     order_time = DateTimeField(required=True)
-
     total_count = IntField(required=True)
     total_price = FloatField(required=True)
 
+    # only app related
+    on_site = BooleanField(required=True)
+    order_status = StringField(required=True)
     ordered_products = ListField(ReferenceField(OrderedProduct, reverse_delete_rule=PULL))
 
     meta = {'collection': 'orders', 'strict': False}

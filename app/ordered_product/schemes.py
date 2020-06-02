ee9638e9d26aac9e6f8d567c3faf7e4f0d004ce5
alex <alekseydavaydosvidaniya@gmail.com>
2020-06-03 00:37:38 +1000

    new fields and schema
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/ordered_product/schemes.py
+++ app/ordered_product/schemes.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel, Field
 
 from app.enums import OrderedProductStatuses
-from app.products.schemes import ProductForCreatedOrder
+from app.products.schemes import ProductForOrder
+from app.utils.custom_types import ObjectIdStr
 
 
 class BaseFields:
-    status: OrderedProductStatuses = Field(..., title='What status of the dish in order')
+    id: ObjectIdStr = Field(title='ID of ordered product')
+    id_required: ObjectIdStr = Field(..., title='ID of ordered product')
+    status: OrderedProductStatuses = Field(title='What status of the dish in order')
+    status_required: OrderedProductStatuses = Field(..., title='What status of the dish in order')
     amount_total: int = Field(1, title='Amount of actual product in order', ge=1)
     amount_ready: int = Field(0, title='Amount ready now', ge=0)
     amount_given: int = Field(0, title='Amount given to client already', ge=0)
@@ -18,20 +22,13 @@ class OrderedProduct(BaseModel):
     amount_ready: int = BaseFields.amount_ready
     amount_given: int = BaseFields.amount_given
     on_site: bool = BaseFields.on_site
-
-    # product = Field()
-
-    class Config:
-        use_enum_values = True
-
-
-class OrderedProductForCreatedOrder(BaseModel):
-    status: OrderedProductStatuses = BaseFields.status
-    amount_total: int = BaseFields.amount_total
-    amount_ready: int = BaseFields.amount_ready
-    amount_given: int = BaseFields.amount_given
-    on_site: bool = BaseFields.on_site
-    product: ProductForCreatedOrder = Field(...)
+    product: ProductForOrder = Field(...)
 
     class Config:
         use_enum_values = True
+
+
+class OrderedProductForOrder(OrderedProduct):
+    class Config:
+        use_enum_values = True
+        orm_mode = True

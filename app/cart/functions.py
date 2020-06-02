1ed975f7c1edb718aa4d07a23e90323aba810ad8
alex <alekseydavaydosvidaniya@gmail.com>
2020-06-03 00:32:23 +1000

    functions stub for cart
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

---
+++ app/cart/functions.py
@@ -0,0 +1,18 @@
+def create_cart(**kwargs):
+    pass
+
+
+def get_cart(**kwargs):
+    pass
+
+
+def add_to_cart(**kwargs):
+    pass
+
+
+def remove_from_cart(**kwargs):
+    pass
+
+
+def delete_cart(**kwargs):
+    pass

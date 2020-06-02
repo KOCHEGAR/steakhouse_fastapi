e226f0fb468254044e7d2c193c8bd21002f32698
alex <alekseydavaydosvidaniya@gmail.com>
2020-06-03 00:32:51 +1000

    schemas for cart
    Signed-off-by: alex <alekseydavaydosvidaniya@gmail.com>

--- app/cart/schemes.py
+++ app/cart/schemes.py
@@ -1,8 +1,35 @@
+from typing import List
+
 from pydantic import BaseModel, Field
+
+from app.ordered_product.schemes import OrderedProductForOrder
+from app.utils.custom_types import ObjectIdStr
+
+
+class BaseFields:
+    id: ObjectIdStr = Field(title='ID of cart')
+    id_required: ObjectIdStr = Field(..., title='ID of cart')
+    total_count: int = Field(title='Total count of items in cart')
+    total_count_required: int = Field(..., title='Total count of items in cart')
+    total_price: float = Field(title='Total price for all items in cart')
+    total_price_required: float = Field(..., title='Total price for all items in cart')
+    ordered_products: List[OrderedProductForOrder] = Field()
+    ordered_products_required: List[OrderedProductForOrder] = Field(...)
+
+
+class AllRequired(BaseModel):
+    total_count: int = BaseFields.total_count_required
+    total_price: float = BaseFields.total_price_required
+    ordered_products: List[OrderedProductForOrder] = BaseFields.ordered_products_required
 
 
 class ResponseCreateCart(BaseModel):
-    id: str = Field(..., title='ID корзины')
-    total_count: int = Field(..., title='Общее количство товара в корзине')
-    total_price: float = Field(..., title='Цена за всё в корзине')
-    # ordered_products =
+    id: ObjectIdStr = BaseFields.id_required
+
+
+class ResponseGetCart(AllRequired):
+    id: ObjectIdStr = BaseFields.id_required
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True

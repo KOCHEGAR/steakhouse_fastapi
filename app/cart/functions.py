fd8ebf22ec2c091aafe03d98a7bca6d4e8238b2d
Alexey Luzin <luzin.a@samberi.com>
2020-06-03 13:34:08 +1000

    setup funcs

--- app/cart/functions.py
+++ app/cart/functions.py
@@ -1,13 +1,38 @@
+from .models import Cart
+from app.utils.mongoengine_helpers import get_model_by_id_or_raise
+from app.products.models import Product
+from app.ordered_product.models import OrderedProduct
+from app.ordered_product.schemes import OrderedProduct as OrderedProductSchema
+from app.products.schemes import ProductForOrder
+
+
 def create_cart(**kwargs):
-    pass
+    return {'id': Cart().save().id}
 
 
 def get_cart(**kwargs):
-    pass
+    return get_model_by_id_or_raise(Cart, kwargs['id'], include_nested=True, nested_deep=2)
 
 
 def add_to_cart(**kwargs):
-    pass
+    # kwargs = {'cart_id': cart_id, 'product_id': product_id, 'quantity': quantity}
+    cart_id: str = kwargs['cart_id']
+    product_id: str = kwargs['product_id']
+    qty: int = kwargs['quantity']
+
+    cart: Cart = get_model_by_id_or_raise(Cart, cart_id, include_nested=True, nested_deep=2)
+    product: Product = get_model_by_id_or_raise(Product, product_id)
+
+    # if not cart.is_product_in_cart(product_id):
+    #     product_dict = dict(ProductForOrder.from_orm(product))
+    #     ordered_product: OrderedProduct = OrderedProduct(amount_total=qty, product=product_dict).save()
+    #     cart.add_new_ordered_product(ordered_product)
+    #     print(dict(OrderedProductSchema.from_orm(ordered_product)))
+    # else:
+    #     pass
+        # cart.modify_ordered_product()
+
+
 
 
 def remove_from_cart(**kwargs):
@@ -15,4 +40,5 @@ def remove_from_cart(**kwargs):
 
 
 def delete_cart(**kwargs):
-    pass
+    cart = get_model_by_id_or_raise(Cart, kwargs['id'])
+    cart.delete()
